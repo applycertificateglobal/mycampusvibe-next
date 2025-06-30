3092edfdd3107af4b5416825659e76208e602fc8
applycertificateglobal <applycertificateglobal@gmail.com>
2025-06-30 21:57:25 +0400

    Add files via upload

---
+++ src/pages/index.jsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div className="p-10 text-center">
+      <h1 className="text-4xl font-bold mb-4">🎓 CampusVibe is Live!</h1>
+      <p className="text-gray-700">Navigate to /university/[id] to test dynamic routes</p>
+    </div>
+  );
+}
